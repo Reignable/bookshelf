1f239ffc76a83b4284cb5ed34d0b1bfb68532d9d
Reignable <joebarret401@gmail.com>
2021-09-20 10:16:41 +0100

    Add dialogs for login and register

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,15 +1,44 @@
-import React from 'react'
+import Dialog from '@reach/dialog'
+import '@reach/dialog/styles.css'
+import {Logo} from 'components/logo'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
-import {Logo} from 'components/logo'
+const DIALOG = {
+  none: 'none',
+  login: 'login',
+  register: 'register',
+}
 
-const App = () => (
-  <>
-    <Logo width="80" height="80" />
-    <h1>Bookshelf</h1>
-    <button onClick={() => alert('You clicked login')}>Login</button>
-    <button onClick={() => alert('You clicked register')}>Register</button>
-  </>
-)
+const App = () => {
+  const [openDialog, setOpenDialog] = useState(DIALOG.none)
+
+  const closeDialog = () => setOpenDialog(DIALOG.none)
+
+  return (
+    <>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <button onClick={() => setOpenDialog(DIALOG.login)}>Login</button>
+      <button onClick={() => setOpenDialog(DIALOG.register)}>Register</button>
+      <Dialog
+        aria-label="Login Form"
+        isOpen={openDialog === DIALOG.login}
+        onDismiss={closeDialog}
+      >
+        <button onClick={closeDialog}>Close</button>
+        <h2>Login</h2>
+      </Dialog>
+      <Dialog
+        aria-label="Register Form"
+        isOpen={openDialog === DIALOG.register}
+        onDismiss={closeDialog}
+      >
+        <button onClick={closeDialog}>Close</button>
+        <h2>Register</h2>
+      </Dialog>
+    </>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
