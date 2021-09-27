07cea8c1d9ef1bd74de59cafabbb13b40364f697
Reignable <joebarret401@gmail.com>
2021-09-27 10:15:36 +0100

    Add ability to send data

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,16 +1,28 @@
+import * as auth from 'auth-provider'
+
 const apiURL = process.env.REACT_APP_API_URL
 
-function client(endpoint, {token, headers: customHeaders, customConfig} = {}) {
+function client(
+  endpoint,
+  {token, headers: customHeaders, data, ...customConfig} = {},
+) {
   const config = {
     method: 'GET',
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
       Authorization: `Bearer ${token}`,
+      'Content-Type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
     ...customConfig,
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      auth.logout()
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate.'})
+    }
     const data = await response.json()
     if (response.ok) {
       return data
