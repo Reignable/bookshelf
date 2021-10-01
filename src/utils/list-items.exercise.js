58e6bd5e787c96d9c31819b434fe433e6dcab1b1
Reignable <joebarret401@gmail.com>
2021-10-01 12:11:53 +0100

    Add optimistic updates

--- src/utils/list-items.exercise.js
+++ src/utils/list-items.exercise.js
@@ -27,27 +27,59 @@ function useListItems(user) {
 
 const defaultMutationOptions = {
   onSettled: () => queryCache.invalidateQueries('list-items'),
+  onError: (err, variables, recover) =>
+    typeof recover === 'function' ? recover() : null,
 }
 
 function useUpdateListItem(user, mutationOptions = {}) {
   return useMutation(
     data =>
       client(`list-items/${data.id}`, {data, method: 'PUT', token: user.token}),
-    {...defaultMutationOptions, ...mutationOptions},
+    {
+      onMutate: newItem => {
+        const previousItems = queryCache.getQueryData('list-items')
+        queryCache.setQueryData('list-items', old => {
+          return old.map(item => {
+            return item.id === newItem.id ? {...item, ...newItem} : item
+          })
+        })
+        return () => queryCache.setQueryData('list-items', previousItems)
+      },
+      ...defaultMutationOptions,
+      ...mutationOptions,
+    },
   )
 }
 
 function useRemoveListItem(user, mutationOptions = {}) {
   return useMutation(
     ({id}) => client(`list-items/${id}`, {token: user.token, method: 'DELETE'}),
-    {...defaultMutationOptions, ...mutationOptions},
+    {
+      onMutate: removedItem => {
+        const previousItems = queryCache.getQueryData('list-items')
+        queryCache.setQueryData('list-items', old =>
+          old.filter(item => item.id !== removedItem.id),
+        )
+        return () => queryCache.setQueryData('list-items', previousItems)
+      },
+      ...defaultMutationOptions,
+      ...mutationOptions,
+    },
   )
 }
 
 function useCreateListItem(user, mutationOptions = {}) {
   return useMutation(
     ({bookId}) => client('list-items', {data: {bookId}, token: user.token}),
-    {...defaultMutationOptions, ...mutationOptions},
+    {
+      onMutate: addedItem => {
+        const previousItems = queryCache.getQueryData('list-items')
+        queryCache.setQueryData('list-items', old => [...old, addedItem])
+        return () => queryCache.setQueryData('list-items', previousItems)
+      },
+      ...defaultMutationOptions,
+      ...mutationOptions,
+    },
   )
 }
 
