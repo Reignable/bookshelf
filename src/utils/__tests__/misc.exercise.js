6b0b3ddd8f8bdec51e0f86c84fe55e9e1e3ae60e
Reignable <joebarret401@gmail.com>
2021-10-25 10:09:36 +0100

    Add test for formatDate

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,5 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from 'utils/misc'
 
+test('formatDate formats the date to look nice', () => {
+  expect(formatDate(new Date(2077, 10, 23))).toMatchInlineSnapshot(`"Nov 77"`)
+})
